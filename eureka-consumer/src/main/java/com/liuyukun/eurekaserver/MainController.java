a993003dace17efb950e029abe3ba4809db69fe6
Lyk19941022 <438396691@qq.com>
2021-02-02 22:36:34 +0800

    111

--- eureka-consumer/src/main/java/com/liuyukun/eurekaserver/MainController.java
+++ eureka-consumer/src/main/java/com/liuyukun/eurekaserver/MainController.java
@@ -1,10 +1,15 @@
 package com.liuyukun.eurekaserver;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,6 +18,13 @@ public class MainController {
 
     @Autowired
     DiscoveryClient client;
+
+    @Autowired
+    EurekaClient client2;
+
+    @Autowired
+    LoadBalancerClient lb;
+
 
     @GetMapping("/getHi")
     public String getHi(){
@@ -44,13 +56,36 @@ public class MainController {
 
     @GetMapping("/client4")
     public Object client4(){
-        List<ServiceInstance> instances = client.getInstances("provider");
-        for (ServiceInstance ins : instances){
+        //通过服务名，找列表
+        List<InstanceInfo> instances = client2.getInstancesByVipAddress("provider", false);
+        for (InstanceInfo ins : instances){
             System.out.println(ins);
         }
         if(instances.size() > 0){
-            ServiceInstance instance = instances.get(0);
+            InstanceInfo instance = instances.get(0);
+            if(instance.getStatus() == InstanceInfo.InstanceStatus.UP){
+                String url = "http://" + instance.getHostName() + ":" + instance.getPort() + "/getHi";
+                System.out.println("url" + url);
+
+                //通过restTemplate调用其他服务接口
+                RestTemplate restTemplate = new RestTemplate();
+                String respStr = restTemplate.getForObject(url, String.class);
+                System.out.println("respStr：" + respStr);
+            }
         }
-        return null;
+        return "xxoo";
+    }
+
+    @GetMapping("/client5")
+    public Object client5(){
+        //通过ribbon完成客户端的负载均衡，过滤掉down了的节点
+        ServiceInstance instance = lb.choose("provider");
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/getHi";
+
+        //通过restTemplate调用其他服务接口
+        RestTemplate restTemplate = new RestTemplate();
+        String respStr = restTemplate.getForObject(url, String.class);
+        System.out.println("respStr：" + respStr);
+        return "xxoo";
     }
 }

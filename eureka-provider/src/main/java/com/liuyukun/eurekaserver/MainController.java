0d9966ca5f47f5ef9bdc96b47cf6306c1f1ccd09
Lyk19941022 <438396691@qq.com>
2021-02-02 22:36:10 +0800

    111

--- eureka-provider/src/main/java/com/liuyukun/eurekaserver/MainController.java
+++ eureka-provider/src/main/java/com/liuyukun/eurekaserver/MainController.java
@@ -1,13 +1,26 @@
 package com.liuyukun.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Autowired
+    HealthStatusService healthStatusSrv;
+
     @GetMapping("/getHi")
     public String getHi(){
         return "Hi";
     }
+
+    @GetMapping("/health")
+    public String health(@RequestParam("status") Boolean status) {
+        //远程控制服务上下线
+        healthStatusSrv.setStatus(status);
+        return healthStatusSrv.getStatus();
+    }
+
 }

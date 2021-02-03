786c780b51beeede59f7e395a860a3d2d0fb0bf5
liuyukun <liuyukun@gitlab.com>
2021-02-03 17:32:53 +0800

    11

--- eureka-provider/src/main/java/com/liuyukun/eurekaserver/MainController.java
+++ eureka-provider/src/main/java/com/liuyukun/eurekaserver/MainController.java
@@ -1,19 +1,26 @@
 package com.liuyukun.eurekaserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class MainController {
+
+    @Value("${server.port}")
+    String port;
 
     @Autowired
     HealthStatusService healthStatusSrv;
 
     @GetMapping("/getHi")
     public String getHi(){
-        return "Hi";
+        return "Hi port:" + port;
     }
 
     @GetMapping("/health")
@@ -23,4 +30,28 @@ public class MainController {
         return healthStatusSrv.getStatus();
     }
 
+    @GetMapping("/getMap")
+    public Map<String,String> getMap(){
+        return Collections.singletonMap("id","100");
+    }
+
+    @GetMapping("/getObj")
+    public Person getObj(){
+        Person person = new Person(100, "xx");
+        return person;
+    }
+
+    @GetMapping("/getObj2")
+    public Person getObj2(String name){
+        Person person = new Person(100, name);
+        return person;
+    }
+
+    @PostMapping("/postLocation")
+    public URI postParam(@RequestBody Person person, HttpServletResponse response) throws Exception {
+
+        URI uri = new URI("https://www.baidu.com/s?wd=" + person.getName().trim());
+        response.addHeader("Location", uri.toString());
+        return uri;
+    }
 }

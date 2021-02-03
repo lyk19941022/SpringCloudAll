f7e05f2c1fe1a77158696fbe7ea5265cbc29251d
liuyukun <liuyukun@gitlab.com>
2021-02-03 17:31:36 +0800

    1

--- eureka-server/src/main/java/com/liuyukun/WebSecurityConfig.java
+++ eureka-server/src/main/java/com/liuyukun/WebSecurityConfig.java
@@ -1,13 +1,13 @@
 package com.liuyukun;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+/*public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,4 +16,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         super.configure(http);
     }
 
-}
+}*/

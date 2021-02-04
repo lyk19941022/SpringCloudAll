a3b069ac6581fb4c16149567a64710d64f57352f
liuyukun <liuyukun@gitlab.com>
2021-02-04 16:08:26 +0800

    spring cloud admin demo

---
+++ admin-demo/src/main/java/com/liuyukun/admindemo/MessageInfo.java
@@ -0,0 +1,14 @@
+package com.liuyukun.admindemo;
+
+public class MessageInfo {
+    private String content;
+    public MessageInfo(String content) {
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
